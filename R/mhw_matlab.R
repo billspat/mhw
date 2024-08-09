b42ced502f75d0f7595772a77442950d96196ce7
Pat Bills <billspat@msu.edu>
2024-08-09 08:16:31 -0400

    saving old R methods for reading Matlab files

---
+++ R/mhw_matlab.R
@@ -0,0 +1,53 @@
+#'test reading MWH output files: mhw_test.R
+
+#' Marine Heat Wave Project
+#' test/exploration reading Dr. Kounta's Matlab output and plotting
+
+library(R.matlab)
+library(sf)
+library(ggplot2)
+
+# CONSTANTS
+
+METRICSFOLDER = '/mnt/research/plz-lab/DATA/ClimateData/MHW_metrics/'
+TESTFILE <-  file.path(metrics_folder,  'ARISE-1.0', 'MHW_metrics_Model_004.mat')
+mhw_matlab2sf <- function(mhwFilePath, mhwStatName = "MHW.max.C"){
+
+    # read matlab
+   mhwStatsMatlab <- readMat(mhwFilePath)
+   # select the column of stats requested
+   s <- mhwStatsMatlab[[mhwStatName]]
+
+   # to make it easy to create lat/lon columns use row/column names
+   rownames(s) <- mhwStatsMatlab$lon
+   colnames(s) <- mhwStatsMatlab$lat
+
+   # convert matrix to long form
+   latlon.df <- data.frame(lat = as.double(rep(colnames(s), each = nrow(s))), 
+                           lon = as.double(rep(rownames(s), ncol(s))), 
+                           x = as.vector(s))
+   
+   # assign col names inlcuding name of stat
+   names(latlon.df)<- c("lat", "lon", mhwStatName)
+   print(paste(nrow(latlon.df), "points"))
+   # convert to geospatial object
+   latlon.sf <- st_as_sf(latlon.df, coords = c('lon', 'lat'), crs=4326)
+   
+   return(latlon.sf)
+   
+}
+
+
+
+quicktest <-function( L1 = '/mnt/research/ibeem/climate_mhw/L1/',
+                      test_folder = 'ARISE-10/2040-2069/max_metrics', 
+                      test_file = 'Model_005_MHW_MAX_int_Year_cat_SSP245.mat', 
+                      mhwStatName = "MHW.max.C") {
+  
+  mhwFilePath <- file.path(L1, test_folder, test_file) 
+  mhw.sf <- mhw_matlab2sf(mhwFilePath, mhwStatName)
+  return(mhw.sf)
+  
+}
+
+
