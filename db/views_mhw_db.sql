a1faf1c09f50d7defa26f54968eb5a0dda72a170
Patrick Bills <billspat@msu.edu>
2024-09-05 09:22:20 -0400

    update view code to use new table names

--- db/views_mhw_db.sql
+++ db/views_mhw_db.sql
@@ -1,35 +1,35 @@
 
 
 
-DROP VIEW IF EXISTS avg_duration_by_decade;
-CREATE VIEW avg_duration_by_decade AS
+DROP VIEW IF EXISTS avg_duration_by_decade_arise10;
+CREATE VIEW avg_duration_by_decade_arise10 AS
 SELECT 
     lat, 
     lon, 
     decade,
     avg(mhw_dur) 
 FROM 
-    mhw_metrics, decades 
+    arise10_metrics, decades 
 WHERE 
     mhw_onset_date >= decades.decade_start and mhw_onset_date <= decades.decade_end
 GROUP BY
    lat, lon, decade
 ;
 
-DROP VIEW IF EXISTS avg_duration_by_decade_truncated;
-CREATE VIEW avg_duration_by_decade_truncated AS
+DROP VIEW IF EXISTS avg_duration_by_decade_truncated_arise10;
+CREATE VIEW avg_duration_by_decade_truncated_arise10 AS
 SELECT 
     lat, 
     lon, 
     decade,
     avg(
-        IF(mhw_metrics.mhw_end_date < decades.decade_end, 
-           mhw_metrics.mhw_end_date - mhw_metrics.mhw_onset_date, 
-           decades.decade_end - mhw_metrics.mhw_onset_date
+        IF(arise10_metrics.mhw_end_date < decades.decade_end, 
+           arise10_metrics.mhw_end_date -arise10_metrics.mhw_onset_date, 
+           decades.decade_end - arise10_metrics.mhw_onset_date
         )
     ) 
 FROM 
-    mhw_metrics, decades 
+    arise10_metrics, decades 
 WHERE 
     mhw_onset_date >= decades.decade_start and mhw_onset_date <= decades.decade_end
 GROUP BY
