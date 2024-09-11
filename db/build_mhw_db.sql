ae01d6e2d67833164d2fa14e47d409d60dedf865
Pat Bills <billspat@msu.edu>
2024-09-11 16:01:16 -0400

    ssp2-4.5 data database import statements

--- db/build_mhw_db.sql
+++ db/build_mhw_db.sql
@@ -136,4 +136,60 @@ create index arise15_end_date_idx on arise15_metrics (mhw_end_date);
 
 create index lat_lon_idx on arise15_metrics (lat, lon);
 
+
+-- ------------
+-- SSP2-4.5 
+
+insert into ensembles values 
+  ('SSP2-4.5', '001'),
+  ('SSP2-4.5', '002'),
+  ('SSP2-4.5', '003'),
+  ('SSP2-4.5', '004'),
+  ('SSP2-4.5', '005'),
+  ('SSP2-4.5', '006'),
+  ('SSP2-4.5', '007'),
+  ('SSP2-4.5', '008'),
+  ('SSP2-4.5', '009'),
+  ('SSP2-4.5', '010');
+  
+create table ssp245_metrics as 
+  SELECT *, '001' AS ensemble, 'SSP2-4.5' AS scenario FROM 'SSP2-4.5/MHW_metrics_Model_001.mat.mhw.csv'
+  UNION
+  SELECT *, '002' AS ensemble, 'SSP2-4.5' AS scenario FROM 'SSP2-4.5/MHW_metrics_Model_002.mat.mhw.csv'
+  UNION
+  SELECT *, '003' AS ensemble, 'SSP2-4.5' AS scenario FROM 'SSP2-4.5/MHW_metrics_Model_003.mat.mhw.csv'
+  UNION
+  SELECT *, '004' AS ensemble, 'SSP2-4.5' AS scenario FROM 'SSP2-4.5/MHW_metrics_Model_004.mat.mhw.csv'
+  UNION
+  SELECT *, '005' AS ensemble, 'SSP2-4.5' AS scenario FROM 'SSP2-4.5/MHW_metrics_Model_005.mat.mhw.csv'
+  UNION
+  SELECT *, '006' AS ensemble, 'SSP2-4.5' AS scenario FROM 'SSP2-4.5/MHW_metrics_Model_006.mat.mhw.csv'
+  UNION
+  SELECT *, '007' AS ensemble, 'SSP2-4.5' AS scenario FROM 'SSP2-4.5/MHW_metrics_Model_007.mat.mhw.csv'
+  UNION
+  SELECT *, '008' AS ensemble, 'SSP2-4.5' AS scenario FROM 'SSP2-4.5/MHW_metrics_Model_008.mat.mhw.csv'
+  UNION
+  SELECT *, '009' AS ensemble, 'SSP2-4.5' AS scenario FROM 'SSP2-4.5/MHW_metrics_Model_009.mat.mhw.csv'
+  UNION
+  SELECT *, '010' AS ensemble, 'SSP2-4.5' AS scenario FROM 'SSP2-4.5/MHW_metrics_Model_010.mat.mhw.csv';
+
+alter table ssp245_metrics add column mhw_onset_date DATE;
+alter table ssp245_metrics add column mhw_end_date DATE;
+
+update ssp245_metrics set mhw_onset_date = cast(strptime(cast(mhw_onset as varchar), '%Y%m%d') as date) , 
+                      mhw_end_date = cast(strptime(cast(mhw_end as varchar), '%Y%m%d') as date);
+
+
+alter table ssp245_metrics add column lat DOUBLE;
+alter table ssp245_metrics add column lon DOUBLE;
+
+update ssp245_metrics set lat = lat_index.lat from lat_index where ssp245_metrics.yloc = lat_index.yloc ;
+update ssp245_metrics set lon = lon_index.lon from lon_index where ssp245_metrics.xloc = lon_index.xloc ;
+
+create index ssp245_onset_date_idx on ssp245_metrics (mhw_onset_date);
+create index ssp245_end_date_idx on ssp245_metrics (mhw_end_date);
+
+create index ssp245_lat_lon_idx on ssp245_metrics (lat, lon);
+
+
 -- create table mclim as select * from 'mclim.csv'
