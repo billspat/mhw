e714eca986e48bd41c1fbbdd826fddf8e932b000
Pat Bills <billspat@msu.edu>
2024-10-03 19:03:08 -0400

    don't need 'decade' column can be imputed from date

--- db/build_mhw_db_ssp245_decade.sql
+++ db/build_mhw_db_ssp245_decade.sql
@@ -3,105 +3,105 @@
 
 
 create table ssp245_decade_metrics as
-SELECT *, '001' AS ensemble, 'SSP-245' AS scenario, 2015 as decade FROM 'SSP-245/2015-2069/MHW_metrics_2015-2024_Model_001.mat.mhw.csv'
+SELECT *, '001' AS ensemble, 'SSP-245' AS scenario, 2015 as decade_name FROM 'SSP-245/2015-2069/MHW_metrics_2015-2024_Model_001.mat.mhw.csv'
  union
-SELECT *, '002' AS ensemble, 'SSP-245' AS scenario, 2015 as decade FROM 'SSP-245/2015-2069/MHW_metrics_2015-2024_Model_002.mat.mhw.csv'
+SELECT *, '002' AS ensemble, 'SSP-245' AS scenario, 2015 as decade_name FROM 'SSP-245/2015-2069/MHW_metrics_2015-2024_Model_002.mat.mhw.csv'
  union
-SELECT *, '003' AS ensemble, 'SSP-245' AS scenario, 2015 as decade FROM 'SSP-245/2015-2069/MHW_metrics_2015-2024_Model_003.mat.mhw.csv'
+SELECT *, '003' AS ensemble, 'SSP-245' AS scenario, 2015 as decade_name FROM 'SSP-245/2015-2069/MHW_metrics_2015-2024_Model_003.mat.mhw.csv'
  union
-SELECT *, '004' AS ensemble, 'SSP-245' AS scenario, 2015 as decade FROM 'SSP-245/2015-2069/MHW_metrics_2015-2024_Model_004.mat.mhw.csv'
+SELECT *, '004' AS ensemble, 'SSP-245' AS scenario, 2015 as decade_name FROM 'SSP-245/2015-2069/MHW_metrics_2015-2024_Model_004.mat.mhw.csv'
  union
-SELECT *, '005' AS ensemble, 'SSP-245' AS scenario, 2015 as decade FROM 'SSP-245/2015-2069/MHW_metrics_2015-2024_Model_005.mat.mhw.csv'
+SELECT *, '005' AS ensemble, 'SSP-245' AS scenario, 2015 as decade_name FROM 'SSP-245/2015-2069/MHW_metrics_2015-2024_Model_005.mat.mhw.csv'
  union
-SELECT *, '006' AS ensemble, 'SSP-245' AS scenario, 2015 as decade FROM 'SSP-245/2015-2069/MHW_metrics_2015-2024_Model_006.mat.mhw.csv'
+SELECT *, '006' AS ensemble, 'SSP-245' AS scenario, 2015 as decade_name FROM 'SSP-245/2015-2069/MHW_metrics_2015-2024_Model_006.mat.mhw.csv'
  union
-SELECT *, '007' AS ensemble, 'SSP-245' AS scenario, 2015 as decade FROM 'SSP-245/2015-2069/MHW_metrics_2015-2024_Model_007.mat.mhw.csv'
+SELECT *, '007' AS ensemble, 'SSP-245' AS scenario, 2015 as decade_name FROM 'SSP-245/2015-2069/MHW_metrics_2015-2024_Model_007.mat.mhw.csv'
  union
-SELECT *, '008' AS ensemble, 'SSP-245' AS scenario, 2015 as decade FROM 'SSP-245/2015-2069/MHW_metrics_2015-2024_Model_008.mat.mhw.csv'
+SELECT *, '008' AS ensemble, 'SSP-245' AS scenario, 2015 as decade_name FROM 'SSP-245/2015-2069/MHW_metrics_2015-2024_Model_008.mat.mhw.csv'
  union
-SELECT *, '009' AS ensemble, 'SSP-245' AS scenario, 2015 as decade FROM 'SSP-245/2015-2069/MHW_metrics_2015-2024_Model_009.mat.mhw.csv'
+SELECT *, '009' AS ensemble, 'SSP-245' AS scenario, 2015 as decade_name FROM 'SSP-245/2015-2069/MHW_metrics_2015-2024_Model_009.mat.mhw.csv'
  union
-SELECT *, '010' AS ensemble, 'SSP-245' AS scenario, 2015 as decade FROM 'SSP-245/2015-2069/MHW_metrics_2015-2024_Model_010.mat.mhw.csv'
+SELECT *, '010' AS ensemble, 'SSP-245' AS scenario, 2015 as decade_name FROM 'SSP-245/2015-2069/MHW_metrics_2015-2024_Model_010.mat.mhw.csv'
  union
-SELECT *, '001' AS ensemble, 'SSP-245' AS scenario, 2025 as decade FROM 'SSP-245/2015-2069/MHW_metrics_2025-2034_Model_001.mat.mhw.csv'
+SELECT *, '001' AS ensemble, 'SSP-245' AS scenario, 2025 as decade_name FROM 'SSP-245/2015-2069/MHW_metrics_2025-2034_Model_001.mat.mhw.csv'
  union
-SELECT *, '002' AS ensemble, 'SSP-245' AS scenario, 2025 as decade FROM 'SSP-245/2015-2069/MHW_metrics_2025-2034_Model_002.mat.mhw.csv'
+SELECT *, '002' AS ensemble, 'SSP-245' AS scenario, 2025 as decade_name FROM 'SSP-245/2015-2069/MHW_metrics_2025-2034_Model_002.mat.mhw.csv'
  union
-SELECT *, '003' AS ensemble, 'SSP-245' AS scenario, 2025 as decade FROM 'SSP-245/2015-2069/MHW_metrics_2025-2034_Model_003.mat.mhw.csv'
+SELECT *, '003' AS ensemble, 'SSP-245' AS scenario, 2025 as decade_name FROM 'SSP-245/2015-2069/MHW_metrics_2025-2034_Model_003.mat.mhw.csv'
  union
-SELECT *, '004' AS ensemble, 'SSP-245' AS scenario, 2025 as decade FROM 'SSP-245/2015-2069/MHW_metrics_2025-2034_Model_004.mat.mhw.csv'
+SELECT *, '004' AS ensemble, 'SSP-245' AS scenario, 2025 as decade_name FROM 'SSP-245/2015-2069/MHW_metrics_2025-2034_Model_004.mat.mhw.csv'
  union
-SELECT *, '005' AS ensemble, 'SSP-245' AS scenario, 2025 as decade FROM 'SSP-245/2015-2069/MHW_metrics_2025-2034_Model_005.mat.mhw.csv'
+SELECT *, '005' AS ensemble, 'SSP-245' AS scenario, 2025 as decade_name FROM 'SSP-245/2015-2069/MHW_metrics_2025-2034_Model_005.mat.mhw.csv'
  union
-SELECT *, '006' AS ensemble, 'SSP-245' AS scenario, 2025 as decade FROM 'SSP-245/2015-2069/MHW_metrics_2025-2034_Model_006.mat.mhw.csv'
+SELECT *, '006' AS ensemble, 'SSP-245' AS scenario, 2025 as decade_name FROM 'SSP-245/2015-2069/MHW_metrics_2025-2034_Model_006.mat.mhw.csv'
  union
-SELECT *, '007' AS ensemble, 'SSP-245' AS scenario, 2025 as decade FROM 'SSP-245/2015-2069/MHW_metrics_2025-2034_Model_007.mat.mhw.csv'
+SELECT *, '007' AS ensemble, 'SSP-245' AS scenario, 2025 as decade_name FROM 'SSP-245/2015-2069/MHW_metrics_2025-2034_Model_007.mat.mhw.csv'
  union
-SELECT *, '008' AS ensemble, 'SSP-245' AS scenario, 2025 as decade FROM 'SSP-245/2015-2069/MHW_metrics_2025-2034_Model_008.mat.mhw.csv'
+SELECT *, '008' AS ensemble, 'SSP-245' AS scenario, 2025 as decade_name FROM 'SSP-245/2015-2069/MHW_metrics_2025-2034_Model_008.mat.mhw.csv'
  union
-SELECT *, '009' AS ensemble, 'SSP-245' AS scenario, 2025 as decade FROM 'SSP-245/2015-2069/MHW_metrics_2025-2034_Model_009.mat.mhw.csv'
+SELECT *, '009' AS ensemble, 'SSP-245' AS scenario, 2025 as decade_name FROM 'SSP-245/2015-2069/MHW_metrics_2025-2034_Model_009.mat.mhw.csv'
  union
-SELECT *, '010' AS ensemble, 'SSP-245' AS scenario, 2025 as decade FROM 'SSP-245/2015-2069/MHW_metrics_2025-2034_Model_010.mat.mhw.csv'
+SELECT *, '010' AS ensemble, 'SSP-245' AS scenario, 2025 as decade_name FROM 'SSP-245/2015-2069/MHW_metrics_2025-2034_Model_010.mat.mhw.csv'
  union
-SELECT *, '001' AS ensemble, 'SSP-245' AS scenario, 2040 as decade FROM 'SSP-245/2015-2069/MHW_metrics_2040-2049_Model_001.mat.mhw.csv'
+SELECT *, '001' AS ensemble, 'SSP-245' AS scenario, 2040 as decade_name FROM 'SSP-245/2015-2069/MHW_metrics_2040-2049_Model_001.mat.mhw.csv'
  union
-SELECT *, '002' AS ensemble, 'SSP-245' AS scenario, 2040 as decade FROM 'SSP-245/2015-2069/MHW_metrics_2040-2049_Model_002.mat.mhw.csv'
+SELECT *, '002' AS ensemble, 'SSP-245' AS scenario, 2040 as decade_name FROM 'SSP-245/2015-2069/MHW_metrics_2040-2049_Model_002.mat.mhw.csv'
  union
-SELECT *, '003' AS ensemble, 'SSP-245' AS scenario, 2040 as decade FROM 'SSP-245/2015-2069/MHW_metrics_2040-2049_Model_003.mat.mhw.csv'
+SELECT *, '003' AS ensemble, 'SSP-245' AS scenario, 2040 as decade_name FROM 'SSP-245/2015-2069/MHW_metrics_2040-2049_Model_003.mat.mhw.csv'
  union
-SELECT *, '004' AS ensemble, 'SSP-245' AS scenario, 2040 as decade FROM 'SSP-245/2015-2069/MHW_metrics_2040-2049_Model_004.mat.mhw.csv'
+SELECT *, '004' AS ensemble, 'SSP-245' AS scenario, 2040 as decade_name FROM 'SSP-245/2015-2069/MHW_metrics_2040-2049_Model_004.mat.mhw.csv'
  union
-SELECT *, '005' AS ensemble, 'SSP-245' AS scenario, 2040 as decade FROM 'SSP-245/2015-2069/MHW_metrics_2040-2049_Model_005.mat.mhw.csv'
+SELECT *, '005' AS ensemble, 'SSP-245' AS scenario, 2040 as decade_name FROM 'SSP-245/2015-2069/MHW_metrics_2040-2049_Model_005.mat.mhw.csv'
  union
-SELECT *, '006' AS ensemble, 'SSP-245' AS scenario, 2040 as decade FROM 'SSP-245/2015-2069/MHW_metrics_2040-2049_Model_006.mat.mhw.csv'
+SELECT *, '006' AS ensemble, 'SSP-245' AS scenario, 2040 as decade_name FROM 'SSP-245/2015-2069/MHW_metrics_2040-2049_Model_006.mat.mhw.csv'
  union
-SELECT *, '007' AS ensemble, 'SSP-245' AS scenario, 2040 as decade FROM 'SSP-245/2015-2069/MHW_metrics_2040-2049_Model_007.mat.mhw.csv'
+SELECT *, '007' AS ensemble, 'SSP-245' AS scenario, 2040 as decade_name FROM 'SSP-245/2015-2069/MHW_metrics_2040-2049_Model_007.mat.mhw.csv'
  union
-SELECT *, '008' AS ensemble, 'SSP-245' AS scenario, 2040 as decade FROM 'SSP-245/2015-2069/MHW_metrics_2040-2049_Model_008.mat.mhw.csv'
+SELECT *, '008' AS ensemble, 'SSP-245' AS scenario, 2040 as decade_name FROM 'SSP-245/2015-2069/MHW_metrics_2040-2049_Model_008.mat.mhw.csv'
  union
-SELECT *, '009' AS ensemble, 'SSP-245' AS scenario, 2040 as decade FROM 'SSP-245/2015-2069/MHW_metrics_2040-2049_Model_009.mat.mhw.csv'
+SELECT *, '009' AS ensemble, 'SSP-245' AS scenario, 2040 as decade_name FROM 'SSP-245/2015-2069/MHW_metrics_2040-2049_Model_009.mat.mhw.csv'
  union
-SELECT *, '010' AS ensemble, 'SSP-245' AS scenario, 2040 as decade FROM 'SSP-245/2015-2069/MHW_metrics_2040-2049_Model_010.mat.mhw.csv'
+SELECT *, '010' AS ensemble, 'SSP-245' AS scenario, 2040 as decade_name FROM 'SSP-245/2015-2069/MHW_metrics_2040-2049_Model_010.mat.mhw.csv'
  union
-SELECT *, '001' AS ensemble, 'SSP-245' AS scenario, 2050 as decade FROM 'SSP-245/2015-2069/MHW_metrics_2050-2059_Model_001.mat.mhw.csv'
+SELECT *, '001' AS ensemble, 'SSP-245' AS scenario, 2050 as decade_name FROM 'SSP-245/2015-2069/MHW_metrics_2050-2059_Model_001.mat.mhw.csv'
  union
-SELECT *, '002' AS ensemble, 'SSP-245' AS scenario, 2050 as decade FROM 'SSP-245/2015-2069/MHW_metrics_2050-2059_Model_002.mat.mhw.csv'
+SELECT *, '002' AS ensemble, 'SSP-245' AS scenario, 2050 as decade_name FROM 'SSP-245/2015-2069/MHW_metrics_2050-2059_Model_002.mat.mhw.csv'
  union
-SELECT *, '003' AS ensemble, 'SSP-245' AS scenario, 2050 as decade FROM 'SSP-245/2015-2069/MHW_metrics_2050-2059_Model_003.mat.mhw.csv'
+SELECT *, '003' AS ensemble, 'SSP-245' AS scenario, 2050 as decade_name FROM 'SSP-245/2015-2069/MHW_metrics_2050-2059_Model_003.mat.mhw.csv'
  union
-SELECT *, '004' AS ensemble, 'SSP-245' AS scenario, 2050 as decade FROM 'SSP-245/2015-2069/MHW_metrics_2050-2059_Model_004.mat.mhw.csv'
+SELECT *, '004' AS ensemble, 'SSP-245' AS scenario, 2050 as decade_name FROM 'SSP-245/2015-2069/MHW_metrics_2050-2059_Model_004.mat.mhw.csv'
  union
-SELECT *, '005' AS ensemble, 'SSP-245' AS scenario, 2050 as decade FROM 'SSP-245/2015-2069/MHW_metrics_2050-2059_Model_005.mat.mhw.csv'
+SELECT *, '005' AS ensemble, 'SSP-245' AS scenario, 2050 as decade_name FROM 'SSP-245/2015-2069/MHW_metrics_2050-2059_Model_005.mat.mhw.csv'
  union
-SELECT *, '006' AS ensemble, 'SSP-245' AS scenario, 2050 as decade FROM 'SSP-245/2015-2069/MHW_metrics_2050-2059_Model_006.mat.mhw.csv'
+SELECT *, '006' AS ensemble, 'SSP-245' AS scenario, 2050 as decade_name FROM 'SSP-245/2015-2069/MHW_metrics_2050-2059_Model_006.mat.mhw.csv'
  union
-SELECT *, '007' AS ensemble, 'SSP-245' AS scenario, 2050 as decade FROM 'SSP-245/2015-2069/MHW_metrics_2050-2059_Model_007.mat.mhw.csv'
+SELECT *, '007' AS ensemble, 'SSP-245' AS scenario, 2050 as decade_name FROM 'SSP-245/2015-2069/MHW_metrics_2050-2059_Model_007.mat.mhw.csv'
  union
-SELECT *, '008' AS ensemble, 'SSP-245' AS scenario, 2050 as decade FROM 'SSP-245/2015-2069/MHW_metrics_2050-2059_Model_008.mat.mhw.csv'
+SELECT *, '008' AS ensemble, 'SSP-245' AS scenario, 2050 as decade_name FROM 'SSP-245/2015-2069/MHW_metrics_2050-2059_Model_008.mat.mhw.csv'
  union
-SELECT *, '009' AS ensemble, 'SSP-245' AS scenario, 2050 as decade FROM 'SSP-245/2015-2069/MHW_metrics_2050-2059_Model_009.mat.mhw.csv'
+SELECT *, '009' AS ensemble, 'SSP-245' AS scenario, 2050 as decade_name FROM 'SSP-245/2015-2069/MHW_metrics_2050-2059_Model_009.mat.mhw.csv'
  union
-SELECT *, '010' AS ensemble, 'SSP-245' AS scenario, 2050 as decade FROM 'SSP-245/2015-2069/MHW_metrics_2050-2059_Model_010.mat.mhw.csv'
+SELECT *, '010' AS ensemble, 'SSP-245' AS scenario, 2050 as decade_name FROM 'SSP-245/2015-2069/MHW_metrics_2050-2059_Model_010.mat.mhw.csv'
  union
-SELECT *, '001' AS ensemble, 'SSP-245' AS scenario, 2060 as decade FROM 'SSP-245/2015-2069/MHW_metrics_2060-2069_Model_001.mat.mhw.csv'
+SELECT *, '001' AS ensemble, 'SSP-245' AS scenario, 2060 as decade_name FROM 'SSP-245/2015-2069/MHW_metrics_2060-2069_Model_001.mat.mhw.csv'
  union
-SELECT *, '002' AS ensemble, 'SSP-245' AS scenario, 2060 as decade FROM 'SSP-245/2015-2069/MHW_metrics_2060-2069_Model_002.mat.mhw.csv'
+SELECT *, '002' AS ensemble, 'SSP-245' AS scenario, 2060 as decade_name FROM 'SSP-245/2015-2069/MHW_metrics_2060-2069_Model_002.mat.mhw.csv'
  union
-SELECT *, '003' AS ensemble, 'SSP-245' AS scenario, 2060 as decade FROM 'SSP-245/2015-2069/MHW_metrics_2060-2069_Model_003.mat.mhw.csv'
+SELECT *, '003' AS ensemble, 'SSP-245' AS scenario, 2060 as decade_name FROM 'SSP-245/2015-2069/MHW_metrics_2060-2069_Model_003.mat.mhw.csv'
  union
-SELECT *, '004' AS ensemble, 'SSP-245' AS scenario, 2060 as decade FROM 'SSP-245/2015-2069/MHW_metrics_2060-2069_Model_004.mat.mhw.csv'
+SELECT *, '004' AS ensemble, 'SSP-245' AS scenario, 2060 as decade_name FROM 'SSP-245/2015-2069/MHW_metrics_2060-2069_Model_004.mat.mhw.csv'
  union
-SELECT *, '005' AS ensemble, 'SSP-245' AS scenario, 2060 as decade FROM 'SSP-245/2015-2069/MHW_metrics_2060-2069_Model_005.mat.mhw.csv'
+SELECT *, '005' AS ensemble, 'SSP-245' AS scenario, 2060 as decade_name FROM 'SSP-245/2015-2069/MHW_metrics_2060-2069_Model_005.mat.mhw.csv'
  union
-SELECT *, '006' AS ensemble, 'SSP-245' AS scenario, 2060 as decade FROM 'SSP-245/2015-2069/MHW_metrics_2060-2069_Model_006.mat.mhw.csv'
+SELECT *, '006' AS ensemble, 'SSP-245' AS scenario, 2060 as decade_name FROM 'SSP-245/2015-2069/MHW_metrics_2060-2069_Model_006.mat.mhw.csv'
  union
-SELECT *, '007' AS ensemble, 'SSP-245' AS scenario, 2060 as decade FROM 'SSP-245/2015-2069/MHW_metrics_2060-2069_Model_007.mat.mhw.csv'
+SELECT *, '007' AS ensemble, 'SSP-245' AS scenario, 2060 as decade_name FROM 'SSP-245/2015-2069/MHW_metrics_2060-2069_Model_007.mat.mhw.csv'
  union
-SELECT *, '008' AS ensemble, 'SSP-245' AS scenario, 2060 as decade FROM 'SSP-245/2015-2069/MHW_metrics_2060-2069_Model_008.mat.mhw.csv'
+SELECT *, '008' AS ensemble, 'SSP-245' AS scenario, 2060 as decade_name FROM 'SSP-245/2015-2069/MHW_metrics_2060-2069_Model_008.mat.mhw.csv'
  union
-SELECT *, '009' AS ensemble, 'SSP-245' AS scenario, 2060 as decade FROM 'SSP-245/2015-2069/MHW_metrics_2060-2069_Model_009.mat.mhw.csv'
+SELECT *, '009' AS ensemble, 'SSP-245' AS scenario, 2060 as decade_name FROM 'SSP-245/2015-2069/MHW_metrics_2060-2069_Model_009.mat.mhw.csv'
  union
-SELECT *, '010' AS ensemble, 'SSP-245' AS scenario, 2060 as decade FROM 'SSP-245/2015-2069/MHW_metrics_2060-2069_Model_010.mat.mhw.csv';
+SELECT *, '010' AS ensemble, 'SSP-245' AS scenario, 2060 as decade_name FROM 'SSP-245/2015-2069/MHW_metrics_2060-2069_Model_010.mat.mhw.csv';
 
 alter table  ssp245_decade_metrics add column mhw_onset_date DATE;
 alter table  ssp245_decade_metrics add column mhw_end_date DATE;
