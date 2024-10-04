4a735f4e04c329048288b872480bbd76b545a60f
Pat Bills <billspat@msu.edu>
2024-10-04 00:06:52 -0400

    clean up draft code files

--- R/mhw_partitioning.R
+++ R/mhw_partitioning.R
@@ -140,12 +140,13 @@ partition_mhw_events_truncated_sql<- function(mhw_table_name, partition_size_yea
   return(gsub("[\n\r]", "", sql))
 }
   
-
+#' mhw_partitioning.R
+#' this is a work in progress (WIP) and not complete
 
 
 #' table of truncated mhw metrics by partitions
 #' 
-#' given set of partions with dates, create table by matching waves to partions and truncated them 
+#' given set of partitions with dates, create table by matching waves to partions and truncated them 
 #' 
 #' @param conn dbi database connection, see mhw_connect()
 #' @param mhw_table_name name of main metrics table to use
@@ -191,4 +192,28 @@ windowing_overlap <- function(vec, seg.length, overlap=0) {
   return(lapply(1:length(starts), function(i) vec[starts[i]:ends[i]]))
 }
 
+
+
+mhw_window_duration <- function(mhwdb_conn, window_size_years=2, truncate=TRUE, start_year = 2040, end_year = 2069){
+  
+  
+  start_years = seq(from = start_year, 
+                    to = end_year,
+                    by = window_size_years)
+  
+  end_years =  seq(from = (start_year+window_size_years-1), 
+                   to = end_year,
+                   by = window_size_years)
+  
+  partition_dates<- data.frame(partition = as.character(start_years), onset = paste0(start_years, "-01-01"), end = paste0(end_years, '-12-31') )
+  
+  create_table_sql = "create table partitions (partition varhcar, onset DATE, end DATE)"
+  
+  
+  sql <- "SELECT lat, lon, decade, avg(mhw_dur) as avg_dur FROM mhw_metrics , decades 
+  WHERE ((mhw_onset_date >= decades.decade_start) AND (mhw_onset_date <= decades.decade_end)) 
+  GROUP BY lat, lon, decade"
+  
+  
+}
 
